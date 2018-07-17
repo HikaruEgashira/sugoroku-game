d4a443045d92df7980e632925b9cf515a09a07bd
HikaruEgashira <shighten@gmail.com>
2018-07-18 07:54:52 +0900

    デバック

--- sugoroku_cui.py
+++ sugoroku_cui.py
@@ -3,6 +3,7 @@ import random
 import numpy as np
 flag_player = False #プレイヤー数決めるときに使う
 flag = False #設定が完了したか＋ゲーム終了
+m = 1
 print('---すごろくゲーム---')
 while flag_player == False:
     player = input('プレイヤー数を入力してください(2~5): ')
@@ -14,6 +15,7 @@ while flag_player == False:
 turn_num = np.ones(n) #ターン数
 total = np.zeros(n) #現在のマス目
 player_pt = np.zeros(n) #現在のポイント
+player_rank = np.zeros(n)
 while flag == False: #20 ~ 40のみを受け付けてint式に
     masu = input('マスの数を入力してください(20~40): ')
     for x in range(20,41): 
@@ -33,22 +35,25 @@ while flag == True:
             print('--------------------')
             print(f'{turn_num[0]}ターン目')
             print(f'現在位置{total}')
+            print(f'得点{player_pt}')
             # print(turn_num)
             while turn_num[_list] == turn_num[0]:
                 print(f'{_list}Pのターンです')
-                turn = input('\'w\'でサイコロを振ります: ')
-                if turn == 'w':
+                turn = True#input('\'w\'でサイコロを振ります: ')
+                if turn == True:
                     dice = random.randint(1,8)
                     print(f'{dice}でした')
                     if dice % 2 == 0:
                         total[_list] += dice
+                        if total[_list] > masu:
+                            total[_list] = 0
                         tt = np.fix(total)##wakaran
                         print(tt)
-                        print(f'{_pt[tt[_list]]}ポイント手に入れた!')
-                        if total[_list] > masu:
-                            total[_list] = 2 * masu - total[_list]
+                        print(f'{_pt[int(tt[_list])]}ポイント手に入れた!')
+                        player_pt[_list] += int(tt[_list])
+                        print(f'{player}P : {player_pt[_list]}Pt')
                     else:
-                        print('奇数なので減点です')
+                        print('奇数なので減点')
                         total[_list] -= dice
                         if total[_list] < 0:
                             total[_list] = 0
@@ -57,6 +62,9 @@ while flag == True:
                     print('Not Accept')
                 if total[_list] == masu:
                     print(f'{total[_list]}! Game Clear!!')
+                    player_pt[_list] += player / m
+                    player_rank[_list] += m
+                    m += 1
                     flag = False
                     clear_turn = np.zeros(n)
                     clear_turn[_list] = turn_num[_list] #クリアターンを保存
@@ -66,6 +74,9 @@ while flag == True:
                 else:
                     print(f'あと{masu - total[_list]}')
     turn_num[0] += 1
-print(f'{total}{turn_num[0]}ターン目で終了')
-clear_turn[0] = np.argmax(clear_turn) + 1
-print(f'{np.argmin(clear_turn)}Pの勝利')
+print(f'ターン数：{turn_num[0]}')
+# clear_turn[0] = np.argmax(clear_turn) + 1
+# print(f'{np.argmin(clear_turn)}Pの勝利')
+print(f'得点：{player_pt}')
+print(f'順位{player_rank}')
+print(f'{np.argmax(player_pt)}Pの勝利')
